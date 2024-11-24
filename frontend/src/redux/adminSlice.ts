594d9ab0afdb228fc1698411a96a32cfeee657a0
Rana718 <ranadolui718@gmail.com>
2024-11-24 23:28:41 +0530

    fix => fix the null value after page refresh

--- frontend/src/redux/adminSlice.ts
+++ frontend/src/redux/adminSlice.ts
@@ -4,11 +4,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 interface AdminState {
     email: string | null;
     token: string | null;
+    tokenType: string | null;
 }
 
 const initialState: AdminState = {
-    email: null,
-    token: null,
+    email: localStorage.getItem('adminEmail'),
+    token: localStorage.getItem('adminToken'),
+    tokenType: localStorage.getItem('adminTokenType'),
 }
 
 
@@ -16,13 +18,21 @@ const adminSlice = createSlice({
     name: 'admin',
     initialState,
     reducers: {
-        loginAdmin(state, action: PayloadAction<{ email: string; token: string }>) {
+        loginAdmin(state, action: PayloadAction<{ email: string; token: string; tokenType: string }>) {
             state.email = action.payload.email;
             state.token = action.payload.token;
+            state.tokenType = action.payload.tokenType;
+            localStorage.setItem('adminEmail', action.payload.email);
+            localStorage.setItem('adminToken', action.payload.token);
+            localStorage.setItem('adminTokenType', action.payload.tokenType);
         },
         logoutAdmin(state) {
             state.email = null;
             state.token = null;
+            state.tokenType = null;
+            localStorage.removeItem('adminEmail');
+            localStorage.removeItem('adminToken');
+            localStorage.removeItem('adminTokenType');
         },
     }
 });
