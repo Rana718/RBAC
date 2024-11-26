077daf70fc337995a981d910bbaf0989dc152e38
Rana718 <ranadolui718@gmail.com>
2024-11-27 01:52:25 +0530

    fix the env error

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosError } from "axios";
 import { User, Role } from "../types";
 
-const API_URL = import.meta.env.API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
+console.log(API_URL);
 
 interface ErrorResponse {
   message: string;
