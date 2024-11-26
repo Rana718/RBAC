347706921874485da158cfe38f82909a93f61d7c
Rana718 <ranadolui718@gmail.com>
2024-11-27 00:45:26 +0530

    marged

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import users, roles
+from db.database import Base, engine
+from routes import mainroute as admin_routes
+
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(users.router, prefix="/users", tags=["users"])
-app.include_router(roles.router, prefix="/roles", tags=["roles"])
-
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the User and Role Management API"}
+    return {"message": "Hello World"}
+
+app.include_router(admin_routes.router, prefix="/admin", tags=["Admin"])
